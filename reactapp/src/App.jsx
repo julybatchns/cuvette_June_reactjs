e40d7ac50421376c655448fe909e30c6e54c720f
webcodeshubham <theshubhamshri@gmail.com>
2024-08-29 21:02:53 +0530

    1st Session

---
+++ reactapp/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="hey">Welcome Everyone! Let's Learn the React JS</h1>
+      <h1>{2 + 3}</h1>
+      <input type="color" name="" id="" />
+      <label htmlFor=""></label>
+    </>
+  );
+}
+
+export default App;
+
+// (JSX) => HTML + JS;
---
+++ reactapp/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+// Save your time
+
+// Less Code
+// Performance
+// Super Faster
+// Simpler Syntax
+// JavaScript Code
+// Virtual DOM
+// Single Page Application
+// JSX
